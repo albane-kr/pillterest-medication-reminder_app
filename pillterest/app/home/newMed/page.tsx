491bcc2fe64d98da40f18ecd7e538bca2a8fb761
albane-kr <riguidelly@gmail.com>
2023-05-03 00:49:26 +0200

    add prescribed med

--- pillterest/app/home/newMed/page.tsx
+++ pillterest/app/home/newMed/page.tsx
@@ -2,15 +2,92 @@
 
 import React from 'react'
 import Link from 'next/link'
+import { Container, Heading, Box, Input, Button, Stack, Select, HStack, useToast} from "@chakra-ui/react";
+import { addMed, toggleMedName } from '@/backend/firebase/firestore/db'
+import { useState } from 'react';
+import { useAuth } from '@/backend/context/authContext';
 
 export default function NewMed() {
+
+  const {isLoggedIn, user} = useAuth()
+  const toast = useToast();
+
+  const [medName, setMedName] = useState("")
+  const [qtyPerTake, setQtyPerTake] = useState("")
+  const [freqPerDay, setFreqPerDay] = useState("")
+  const [timeOfTreatment, setTimeOfTreatment] = useState("")
+  //const [stockLeft, setStockLeft] = useState("")
+  
+  const handleAddMed = async () => {
+
+    if (!isLoggedIn) {
+      toast({
+        title: "You must be logged in to add a prescribed medication",
+        status: "error",
+        duration: 9000,
+        isClosable: true,
+      })
+      return;
+    }
+  
+    const addNewMed = { 
+      medName, 
+      qtyPerTake, 
+      freqPerDay, 
+      timeOfTreatment, 
+      //stockLeft,
+      //userId : user.uid 
+    }
+
+    await addMed(addNewMed)
+    setMedName("")
+    setQtyPerTake("")
+    setFreqPerDay("")
+    setTimeOfTreatment("")
+    /*setStockLeft("")*/
+    toast({
+      title: "Prescribed medication created successfully",
+      status: "success"
+    })
+  }
+
   return (
-    <>
-      <div>
-        here will be the form to add a new med
-      </div>
-      <Link href="/newMed/createMed">Go to Create Med Form Page</Link>
-      <Link href="/">Link to Home Page</Link>
-    </>
+    <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '0', sm: '8' }}>
+      <Stack spacing="8">
+        <Heading>Describe your new medication</Heading>
+      </Stack>
+      <Box
+        py={{ base: '0', sm: '8' }}
+        px={{ base: '4', sm: '10' }}
+        bg={{ base: 'transparent', sm: 'bg-surface' }}
+        boxShadow={{ base: 'none', sm: 'md' }}
+        borderRadius={{ base: 'none', sm: 'xl' }}
+      >
+        <Stack direction='column'>
+          <HStack spacing={4}>
+            <Select placeholder='Name of medication' value={medName} onChange={(e) => setMedName(e.target.value)}>
+              <option value={"name1"}>name1</option>
+              <option value={"name2"}>name2</option>
+            </Select>
+            <Button>
+              <Link href="/home/newMed/createMed/">
+                +
+              </Link>
+            </Button>
+          </HStack>
+          <Input placeholder='Quantity per take' value={qtyPerTake} onChange={(e) => setQtyPerTake(e.target.value)} />
+          <Input placeholder='Frequency per day' value={freqPerDay} onChange={(e) => setFreqPerDay(e.target.value)} />
+          <Input placeholder='Time of treatment' value={timeOfTreatment} onChange={(e) => setTimeOfTreatment(e.target.value)} />
+          <Button onClick={() => handleAddMed()} disabled={freqPerDay.length < 1 || toggleMedName.length < 1 || qtyPerTake.length < 1 || timeOfTreatment.length < 1}>
+            Add        
+          </Button>
+          <Button>
+            <Link href="/home/">
+              Finish
+            </Link>
+          </Button>
+        </Stack>
+      </Box>
+    </Container>
   )
 }
