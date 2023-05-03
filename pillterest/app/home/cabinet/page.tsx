931b8642a4675510276fc1fece6f560d7c4909c7
albane-kr <riguidelly@gmail.com>
2023-05-03 09:48:14 +0200

    cabinet

--- pillterest/app/home/cabinet/page.tsx
+++ pillterest/app/home/cabinet/page.tsx
@@ -1,13 +1,50 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
-import { Box, IconButton, Button, Card, CardHeader, CardBody, CardFooter, Checkbox, InputGroup, InputRightElement, Skeleton, Container, Heading, VStack, StackDivider, HStack, Input, Stack, Text } from '@chakra-ui/react'
+import { Box, IconButton, Button, SimpleGrid, Card, CardHeader, CardBody, CardFooter, Checkbox, InputGroup, InputRightElement, Skeleton, Container, Heading, VStack, StackDivider, HStack, Input, Stack, Text, useToast, Badge } from '@chakra-ui/react'
 import { DeleteIcon } from '@chakra-ui/icons'
+import { db } from "@/backend/firebase/firebaseConfig";
+import { collection, onSnapshot, query, QuerySnapshot, where } from "firebase/firestore";
+import { deleteMed } from '@/backend/firebase/firestore/db'
+import { useAuth } from '@/backend/context/authContext'
 
 export default function Cabinet() {
+
+  const [med, setMed] = useState([])
+  const { user } = useAuth()
+  const toast = useToast()
+
+  const refreshData = () => {
+    if (!user) {
+      setMed([])
+      return
+    }
+
+    const q = query(collection(db, "Prescribed_Med"), where("user", "==", user.uid)).get()
+    onSnapshot(q, (querySnapshot) => {
+      let array = []
+      querySnapshot.docs.forEach((doc) => {
+        array.push({ id: doc.id, ...doc.data() })
+      })
+      setMed(array)
+    })
+  }
+
+  useEffect(() => {
+    refreshData()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [ user])
+
+  const handleMedDelete = async (id) => {
+    if (confirm("Are you sure you want to delete this medication ?")) {
+      deleteMed(id)
+      toast({ title: "Prescribed medication deleted successfully", status:"success"})
+    }
+  }  
+
   return (
-    <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '4', sm: '8' }}>
+    <Container maxW="lg" py={{ base: 'md', md: 'lg' }} px={{ base: 'sm', sm: 'md' }}>
       <Stack spacing="8">
         <Stack spacing="8" textAlign="center">
           <Heading size={{ base: 'md', md: 'lg' }}>
@@ -25,53 +62,26 @@ export default function Cabinet() {
           <Heading size={{ base: 'sd', md: 'md' }}>
             Your current treatment
           </Heading>
-          <Card>
-            <CardHeader>
-              <HStack spacing='12'>
-              <Heading size="sm">Name of med 1</Heading>
-              <InputGroup>
-                <InputRightElement>
-                  <IconButton variant='ghost' colorScheme='black' aria-label='Delete medication' icon={<DeleteIcon />} />
-                </InputRightElement>
-              </InputGroup>
-              </HStack>
-            </CardHeader>
-            <CardBody>
-              <Text>Quantity</Text>
-              <Text>Frequency</Text>
-              <Text>Time of treatment</Text>
-              <Text>Stock left</Text>
-            </CardBody>
-          </Card>
-          <Card>
-            <CardHeader>
-              <HStack spacing='12'>
-              <Heading size="sm">Name of med 2</Heading>
-              <InputGroup>
-                <InputRightElement>
-                  <IconButton variant='ghost' colorScheme='black' aria-label='Delete medication' icon={<DeleteIcon />} />
-                </InputRightElement>
-              </InputGroup>
-              </HStack>
-            </CardHeader>
-            <CardBody>
-              <Text>Quantity</Text>
-              <Text>Frequency</Text>
-              <Text>Time of treatment</Text>
-              <Text>Stock left</Text>
-            </CardBody>
-          </Card>
+          <Box mt={6}>
+            <SimpleGrid column={{base: 'sm', sm: 'md'}} spacing={8}>
+              {med && med.map((addNewMed)) => (
+                <Box>
+                  <Heading>
+                    {addNewMed.medName}{" "}
+                    <Badge color="blue" onClick={() => handleMedDelete(addNewMed.id)}>
+                      <DeleteIcon />
+                    </Badge>
+                  </Heading>
+                  <Text>Quantity: {addNewMed.qtyPerTake}</Text>
+                  <Text>Frequency: {addNewMed.freqPerDay}</Text>
+                  <Text>Time of treatment: {addNewMed.timeOfTreatment}</Text>
+                </Box>
+              )}
+            </SimpleGrid>
+          </Box>
         </Stack>
 
       </Stack>
     </Container>
   )
 }
-
-/*<>
-      <div>
-        here will be the cabinet
-      </div>
-      <Link href="/cabinet/stock">Go to Stock Page</Link>
-      <Link href="/">Link to Home Page</Link>
-    </>*/
