bed76cfbdaf61619243fe3e1c12e06d67a9d2e46
albane-kr <riguidelly@gmail.com>
2023-05-16 12:04:28 +0200

    deleted code related to history events created fctto control the notifications to display

--- pillterest/app/home/page.jsx
+++ pillterest/app/home/page.jsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/navigation'
 import { useEffect, useState } from 'react'
 import { auth } from '@/backend/firebase/firebaseConfig'
 import { db } from "@/backend/firebase/firebaseConfig";
-import { collection, query, where, getDocs, orderBy, doc, } from "firebase/firestore";
+import { collection, query, where, getDocs } from "firebase/firestore";
 import { FaSignOutAlt } from 'react-icons/fa'
 import { historyNotif, updateNotification } from '@/backend/firebase/firestore/db'
 
@@ -23,36 +23,6 @@ export default function Home() {
   const logout = () => {
     auth.signOut()
   }
-
-  const refreshData = async () => {
-    if (!user) {
-      setNotification([])
-      return
-    }
-
-    //get unchecked notifications of the day
-    const collNotifications = collection(db, "Notifications")
-    const querySnapshotUncheckedNotif = await getDocs(query(collNotifications, where("uid", "==", user.uid)))
-    let arrayUncheckedNotif = []
-    querySnapshotUncheckedNotif.forEach((docSnapUncheckedNotif) => {
-      arrayUncheckedNotif.push({ id: docSnapUncheckedNotif.id, ...docSnapUncheckedNotif.data() })
-      console.log("uncheckedNotif: ", { id: docSnapUncheckedNotif.id, ...docSnapUncheckedNotif.data() })
-      setNotificationUnchecked(arrayUncheckedNotif)
-      //console.log(docSnapUncheckedNotif.data().timeTreatmentStart)
-    })
-
-    //get checked notifications of the day
-    const collHistoryNotif = collection(db, "History_Notifications")
-    const querySnapshotHistoryNotif = await getDocs(query(collHistoryNotif, where("uid", "==", user.uid)))
-    let arrayHistoryNotif = []
-    querySnapshotHistoryNotif.forEach((docSnapHistoryNotif) => {
-      arrayHistoryNotif.push({ id: docSnapHistoryNotif.id, ...docSnapHistoryNotif.data() })
-      console.log("checkedNotif: ", { id: docSnapHistoryNotif.id, ...docSnapHistoryNotif.data() })
-      setNotificationChecked(arrayHistoryNotif)
-      console.log(docSnapHistoryNotif.data().date)
-    })
-  }
-
 
   //converts number to string 
   //then pads it in a 2 digits string by adding 0 in front of the number if it is only 1 digit long
@@ -70,27 +40,45 @@ export default function Home() {
   const dateToday = formatDate(new Date());
   console.log(dateToday)
 
+  function notificationCondition(notif) {
+    return(
+      !notif.date.includes(dateToday)
+    && notif.timeTreatmentStart.substr(0,10) <= dateToday 
+    && notif.timeTreatmentEnd.substr(0,10) >= dateToday)
+  }
 
-  const handleUpdateNotif = async (docId, medName) => {
-    const dataToUpdate = {
-      date: dateToday
+  const refreshData = async () => {
+    if (!user) {
+      setNotification([])
+      return
     }
-    updateNotification(docId, dataToUpdate)
+
+    //get unchecked notifications of the day
+    const collNotifications = collection(db, "Notifications")
+    const querySnapshotUncheckedNotif = await getDocs(query(collNotifications, where("uid", "==", user.uid)))
+    let arrayUncheckedNotif = []
+    querySnapshotUncheckedNotif.forEach((docSnapUncheckedNotif) => {
+      if ( notificationCondition(docSnapUncheckedNotif.data())){
+      arrayUncheckedNotif.push({ id: docSnapUncheckedNotif.id, ...docSnapUncheckedNotif.data() })
+      console.log("uncheckedNotif: ", { id: docSnapUncheckedNotif.id, ...docSnapUncheckedNotif.data() })
+      setNotificationUnchecked(arrayUncheckedNotif)
+      }
+      //console.log(docSnapUncheckedNotif.data().timeTreatmentStart)
+    })
+  }
+
+  const handleUpdateNotif = async (doc, medName) => {
+    const dataToUpdate = {
+      date: [...doc.date, dateToday]
+    }
+    updateNotification(doc.id, dataToUpdate)
     toast({
       title: "Date added with success",
       status: "success"
     })
 
-    const dateHistory = {
-      medicationName: medName,
-      uid: user.uid,
-      date: [dateToday]
-    }
-    historyNotif(docId, dateHistory)
-    toast({
-      title: "History updated with success",
-      status: "success"
-    })
+    //to update database
+    refreshData()
   }
 
   useEffect(() => {
@@ -100,7 +88,8 @@ export default function Home() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router, user])
 
-var i=0
+
+
   return (
     <Container maxW="lg" py={{ base: '12', md: '24' }} px={{ base: '4', sm: '8' }} backgroundColor='purple.50'>
       <VStack spacing={5} align='stretch' s='purple.100'>
@@ -131,46 +120,30 @@ var i=0
           <VStack spacing={5} align='stretch' divider={<StackDivider borderColor='gray.500' />}>
             {notificationUnchecked && notificationUnchecked.map((docSnapUncheckedNotif) =>
               //{if(docSnapUncheckedNotif.timeTreatmentStart.substr(0, 10) <= dateToday && docSnapUncheckedNotif.timeTreatmentEnd.substr(0, 10) >= dateToday) {
-              <Box key={docSnapUncheckedNotif.id} h='40px'>
+              <Box key={docSnapUncheckedNotif.id} h='50px'>
                 <Heading size='xs'>{docSnapUncheckedNotif.medicationName}</Heading>
                 <InputGroup>
                   <InputRightElement>
                     <Checkbox
                       checked
-                      isDisabled={docSnapUncheckedNotif.date.substr(0, 10) == dateToday}
+                      isDisabled={docSnapUncheckedNotif.date.includes(dateToday)}
                       onChange={() => {
-                        if (docSnapUncheckedNotif.date.substr(0, 10) !== dateToday && docSnapUncheckedNotif.timeTreatmentStart.substr(0, 10) <= dateToday && docSnapUncheckedNotif.timeTreatmentEnd.substr(0, 10) >= dateToday ) {
-                          handleUpdateNotif(docSnapUncheckedNotif.id, docSnapUncheckedNotif.medicationName)
+                        if (notificationCondition(docSnapUncheckedNotif)) {
+                          handleUpdateNotif(docSnapUncheckedNotif, docSnapUncheckedNotif.medicationName)
+                          refreshData()
                         } else {
                           toast({title:"conditions don't match", status:"error"})
                         }
                       }}
                       borderColor='black'
                     ></Checkbox>
-                    {console.log(docSnapUncheckedNotif.date.substr(0, 10))}
                   </InputRightElement>
                 </InputGroup>
-                <Text size='xs'>I took {docSnapUncheckedNotif.frequencyPerDay} times the medication</Text>
+                <Text size='xs'>I took {docSnapUncheckedNotif.quantityPerTake} times per take</Text>
+                <Text size='xs'>I took {docSnapUncheckedNotif.frequencyPerDay} times the medication today</Text>
               </Box>
               //}}
             )}
-            {notificationChecked && notificationChecked.map((docSnapHistoryNotif) =>
-              //how can I check if the date is == to an item ?
-              //{if(dateToday == docSnapHistoryNotif.date){
-              <Box key={docSnapHistoryNotif.id} h='40px'>
-                <Heading size='xs'>{docSnapHistoryNotif.medicationName}</Heading>
-                <InputGroup>
-                  <InputRightElement>
-                    <Checkbox
-                      defaultChecked={docSnapHistoryNotif.date == dateToday}
-                      borderColor='black'
-                    ></Checkbox>
-                  </InputRightElement>
-                </InputGroup>
-                </Box>
-              //}}
-            )}
-
           </VStack>
         </Box>
         <Stack spacing="6">
